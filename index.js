1ced5fc07f1a88e6f9de8cfbfcc5d5536883ce62
dachieu2801 <ankhieu322@gmail.com>
2023-10-27 17:37:37 +0700

    first

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const port = 5000
+const port = process.env.PORT 5000
 
 //route
 const route = require('./routes');
--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,7 @@ router.post('/signin', UserController.signin);
 router.put('/editUser', UserController.editUser);
 router.get('/admin/:id', isAuth.admin, UserController.detailUser);
 router.get('/admin', isAuth.admin, UserController.collectUsers);
-router.get('/', UserController.allUsers);
+router.get('/', isAuth.admin, UserController.allUsers);
 
 
 module.exports = router;
