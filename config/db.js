6de2d375a104a50fce3bb34b67adebed1da22549
dachieu2801 <ankhieu322@gmail.com>
2023-10-27 18:00:19 +0700

    first

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-const URI = process.env.MONGODB_CONNECT_URI || 'mongodb://127.0.0.1:27017/assign3'
-// const URI = 'mongodb+srv://ankhieu322:j5GERTe5oUWil3Jn@cluster0.5euclk4.mongodb.net/assign3?retryWrites=true&w=majority'
+  // const URI = 'mongodb+srv://ankhieu322:j5GERTe5oUWil3Jn@cluster0.5euclk4.mongodb.net/assign3?retryWrites=true&w=majority'
 
   async function main() {
     try {
-      await mongoose.connect(URI);
+      await mongoose.connect(process.env.MONGODB_CONNECT_URI || 'mongodb://127.0.0.1:27017/assign3');
       console.log('success');
 
     } catch (err) {
